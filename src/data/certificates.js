71e5d756b7dbab85faa9a0661977256d3d7ff464
sarachreimm <shreim.sara@gmail.com>
2025-02-12 20:48:26 +0200

    data update 2

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -6,6 +6,13 @@ import iso14001 from '../assets/certificates/medcables_14001-2015.png';
 import iso45001 from '../assets/certificates/medcables_45001-2018.png';
 import iso9001 from '../assets/certificates/ISO9001.png';
 import EDLiban5117 from '../assets/certificates/EDLiban5117.png';
+import MOE52360 from '../assets/certificates/MOE52360.png';
+import CertofConformity from '../assets/certificates/CertofConformity.png';
+import typetest from '../assets/certificates/typetest.png';
+import iri from '../assets/certificates/iri.png';
+import iri2 from '../assets/certificates/iri2.png';
+import testTUV from '../assets/certificates/testTUV.png';
+import qualiconsult from '../assets/certificates/qualiconsult.png';
 
 import edliban6101pdf from '../assets/certificates/EDLiban6101.pdf';
 import edliban6102pdf from '../assets/certificates/EDLiban6102.pdf';
@@ -15,6 +22,13 @@ import iso14001pdf from '../assets/certificates/medcables_14001-2015.pdf';
 import iso45001pdf from '../assets/certificates/medcables_45001-2018.pdf';
 import iso9001pdf from '../assets/certificates/ISO9001.pdf';
 import EDLiban5117pdf from '../assets/certificates/EDLiban5117.pdf';
+import MOE52360pdf from '../assets/certificates/MOE52360.pdf';
+import CertofConformitypdf from '../assets/certificates/CertofConformity.pdf';
+import typetestpdf from '../assets/certificates/typetest.pdf';
+import iripdf from '../assets/certificates/iri.pdf';
+import iri2pdf from '../assets/certificates/iri2.pdf';
+import testTUVpdf from '../assets/certificates/testTUV.pdf';
+import qualiconsultpdf from '../assets/certificates/qualiconsult.pdf';
 
 export const certificates = [
     {
@@ -80,5 +94,62 @@ export const certificates = [
       preview: EDLiban5117,
       file: EDLiban5117pdf,
       color: '#2ecc71'
+    },
+    {
+      id: 9,
+      title: 'Classifications in Iraq',
+      issuer: 'Ministry of Electricity',
+      preview: MOE52360,
+      file: MOE52360pdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 10,
+      title: 'Certificate of Conformity for Goods Exported to Kurdistan',
+      issuer: 'intertek',
+      preview: CertofConformity,
+      file: CertofConformitypdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 11,
+      title: 'Type Test IEC',
+      issuer: 'IEC',
+      preview: typetest,
+      file: typetestpdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 12,
+      title: 'IRI Test for Cables',
+      issuer: 'IRI',
+      preview: iri,
+      file: iripdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 13,
+      title: 'IRI Test Report Iraq',
+      issuer: 'IRI',
+      preview: iri2,
+      file: iri2pdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 14,
+      title: 'Test by TUV',
+      issuer: 'TUV',
+      preview: testTUV,
+      file: testTUVpdf,
+      color: '#2ecc71'
+    },
+    {
+      id: 15,
+      title: 'Sarafand Hospital Electrical Cables Tests Witnessing Report',
+      issuer: 'Qualiconsult',
+      preview: qualiconsult,
+      file: qualiconsultpdf,
+      color: '#2ecc71'
     }
+
   ];
