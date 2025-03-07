84ffe0dd886606399563789e9c1daedc052a9309
sarachreimm <sara.chreim@intelligencia.fr>
2025-03-07 11:00:03 +0100

    about us images update

--- src/components/CompanyInfoSection/CompanyInfoSection.js
+++ src/components/CompanyInfoSection/CompanyInfoSection.js
@@ -1,46 +1,35 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
 import "./CompanyInfoSection.css";
-import companyImage from "../../assets/images/aa.jpeg";
 import { company_info } from "../../data/company_info";
 
+const images = [
+  require("../../assets/images/IMG_7494.JPG"),
+  require("../../assets/images/IMG_7503.JPG"),
+  require("../../assets/images/IMG_7506.JPG"),
+  require("../../assets/images/hero2.jpeg"),
+];
+
 const CompanyInfo = () => {
+  const [bgIndex, setBgIndex] = useState(0);
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setBgIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+  
+      return () => clearInterval(interval);
+    }, []);
   const { the_four_features, about_description } = company_info[0];
   return (
     <section className="company-info">
       <div className="info-container">
         <div className="info-content">
           <div className="info-text">
-            <motion.h2
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8 }}
-            >
-              About Med Cables Company
-            </motion.h2>
-            <motion.p
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.2, duration: 0.8 }}
-            >
-              {about_description}
-            </motion.p>
+            <h2>About Med Cables Company</h2>
+            <p>{about_description}</p>
           </div>
-          <motion.div
-            className="info-image"
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0.6, duration: 0.8 }}
-          >
-            <img src={companyImage} alt="Med Cables Company" />
-          </motion.div>
+          <div className="info-image" style={{ backgroundImage: `url(${images[bgIndex]})` }}></div>
         </div>
-        <motion.div
-          className="info-grid"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.8, duration: 0.8 }}
-        >
+        <div className="info-grid" >
           <div className="info-item">
             <div className="feature-icon">{the_four_features[0].icon}</div>
             <h3>{the_four_features[0].title}</h3>
@@ -61,7 +50,7 @@ const CompanyInfo = () => {
             <h3>{the_four_features[3].title}</h3>
             <p>{the_four_features[3].desc}</p>
           </div>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import "./Hero.css";
 
 const backgrounds = [
-  // require("../../assets/images/hero-bg.jpg"),
-  require("../../assets/images/hero4.JPG"),
-  require("../../assets/images/hero3.jpeg"),
+  require("../../assets/images/hero1.JPG"),
+  require("../../assets/images/hero2.jpeg"),
 ];
 
 const HeroSection = () => {
@@ -14,7 +12,7 @@ const HeroSection = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setBgIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
-    }, 5000); // Change image every 5 seconds
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
@@ -23,23 +21,13 @@ const HeroSection = () => {
     <section className="hero-section" >
     <section className="hero-background" style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }}></section>
       <div className="hero-main">
-        <motion.h1
-          className="hero-title"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-        >
+        <h1 className="hero-title">
           <span>Mediterranean Cables</span>
-        </motion.h1>
-        <motion.div 
-          className='hero-content'
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-        >
+        </h1>
+        <div className='hero-content'>
           <h2>Quality Cables Since 2009</h2>
           <p>A leading cable manufacturer that delivers reliable and high-quality customized solutions for various industries.</p>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
