9f48d4e8f5587060ad878a97f94c0688f44b119f
sarachreimm <sara.chreim@intelligencia.fr>
2025-03-10 22:39:21 +0100

    hero images

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Hero.css";
 
 const backgrounds = [
-  require("../../assets/images/hero1.JPG"),
+  require("../../assets/images/hero3.jpg"),
   require("../../assets/images/hero2.jpeg"),
   require("../../assets/images/IMG_7503.JPG"),
   require("../../assets/images/IMG_7504.JPG"),
@@ -20,15 +20,21 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section className="hero-section" >
-    <section className="hero-background" style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }}></section>
+    <section className="hero-section">
+      <section
+        className="hero-background"
+        style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }}
+      ></section>
       <div className="hero-main">
-        <div className='hero-content'>
-        <h1 className="hero-title">
-          <span>Mediterranean Cables</span>
-        </h1>
+        <div className="hero-content">
+          <h1 className="hero-title">
+            <span>Mediterranean Cables</span>
+          </h1>
           <h2>Quality Cables Since 2009</h2>
-          <p>A leading cable manufacturer that delivers reliable and high-quality customized solutions for various industries.</p>
+          <p>
+            A leading cable manufacturer that delivers reliable and high-quality
+            customized solutions for various industries.
+          </p>
         </div>
       </div>
     </section>
