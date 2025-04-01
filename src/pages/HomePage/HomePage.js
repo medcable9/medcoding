94c0645e16087513ca71e26cf07fc90d2b5b27e5
sarachreimm <sara.chreim@intelligencia.fr>
2025-04-01 15:27:32 +0200

    deleted featured product component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,6 @@ import HeroSection from '../../components/Hero/Hero';
 import ClientShowcase from "../../components/ClientShowcase/ClientShowcase";
 import Services from "../../components/CommonServices/CommonServices"
 import Activities from "../../components/Activities/Activities"
-import FeaturedProduct from '../../components/FeaturedProduct/FeaturedProduct';
 import TopMachines from '../../components/TopMachines/TopMachines';
 
 import './HomePage.css';
